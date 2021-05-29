9c52053c46812bf4004827c74693f704fe690737
Alessandro Annini <alessandro.annini@gmail.com>
2021-05-29 09:37:00 +0200

    style: :art: add some space between section

--- signupbackend/index.js
+++ signupbackend/index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const routesUrls = require('./routes/routes');
 const cors = require('cors');
+
 dotenv.config();
+
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
   console.log('database connected')
 );
