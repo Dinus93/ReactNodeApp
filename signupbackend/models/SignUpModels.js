cb2a7c7596219dd4b79da1d6cf25a9650fd6cc7e
Alessandro Annini <alessandro.annini@gmail.com>
2021-05-29 10:01:53 +0200

    fix: :bug: change property name on db model

--- signupbackend/models/SignUpModels.js
+++ signupbackend/models/SignUpModels.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
 const SignUpSchema = new Schema({
   firstName: { type: String, required: true },
-  surName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const SignUpModel = mongoose.model("SignUpModel", SignUpSchema);
+const SignUpModel = mongoose.model('SignUpModel', SignUpSchema);
 
 module.exports = SignUpModel;
