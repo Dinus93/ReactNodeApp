085bb366ef683c61abcfb604376d6ad826b28e83
Alessandro Annini <alessandro.annini@gmail.com>
2021-05-29 09:38:49 +0200

    style: :art: add some space between section

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,21 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const signUpModelCopy = require("../models/SignUpModels");
-router.post("/signup", (request, response) => {
+const signUpModelCopy = require('../models/SignUpModels');
+
+router.post('/signup', (request, response) => {
   const signedUpUser = new signUpModelCopy({
     firstName: request.body.firstName,
     surName: request.body.surName,
     email: request.body.email,
     password: request.body.password,
   });
+
   signedUpUser
     .save()
-    .then((data) => {
-      response.json(data);
-    })
-    .catch((error) => {
-      response.json(error);
-    });
+    .then((data) => response.json(data))
+    .catch((error) => response.json(error));
 });
 
 module.exports = router;
